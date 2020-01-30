d82fb2fcec4892e19a012fe9bef5b94da8a74eab
jull <50972782+sirajul777@users.noreply.github.com>
2020-01-30 14:19:36 +0800

    upload file
    upload basic file

---
+++ vue/script.js
@@ -0,0 +1,13 @@
+var shoppingList = new Vue({
+    el: '#shopping-list',
+    data: {
+        header: 'Vue is ready to party',
+    }
+})
+
+var titlename = new Vue({
+    el: '#titlename',
+    data: {
+        title: 'My Apps',
+    }
+})
